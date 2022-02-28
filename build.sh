8887ba1ecc1bc6bd17308a865b2e73de5f4b569b
Emil Soleyman-Zomalan <emil@soleyman.com>
2022-02-28 14:12:40 -0600

    Change the output files per naming convention
    Change from syr to syr_SY

--- build.sh
+++ build.sh
@@ -13,11 +13,11 @@ sort -u dict/sureth.dic dict/sedra.dic > release/syr.tmp.dic
 sed -i -e '/^$/d' release/syr.tmp.dic
 
 # Add line count
-wc -l < release/syr.tmp.dic > release/syr.dic
+wc -l < release/syr.tmp.dic > release/syr_SY.dic
 
 # Copy the temporary dictionary to the release along with its affix file
 cat release/syr.tmp.dic >> release/syr.dic
-cat dict/syr.aff > release/syr.aff
+cat dict/syr.aff > release/syr_SY.aff
 
 # Remove temporary dictionary
 rm release/syr.tmp.dic
